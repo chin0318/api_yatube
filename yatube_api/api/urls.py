59e6527eb4c0693f14f65f69b27b92e00b4c0d76
Дмитрий <dmitry.osipov@moex.com>
2022-02-14 14:22:00 +0300

    Спринт8 Версия1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,7 @@
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-
-from api.views import CommentViewSet, GroupViewSet, PostViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register("posts", PostViewSet, basename="posts")
